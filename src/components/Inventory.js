045741d11da06e5957ba2b7362bd086d6bbfb0bf
Ryan Aldous <aldous.ryan1@gmail.com>
2020-05-02 19:48:36 -0400

    responsiveness

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -5,10 +5,10 @@ const Inventory = ( { bells, turnips } ) => {
     return (
         <div>
             <div className='container'>
-            <img src='https://www.models-resource.com/resources/big_icons/36/35107.png' height='300em' width='350em'/>
-            <div className='bellText'>{bells}</div>
-            <img src='https://wyspstore2.s3.amazonaws.com/thumbs/823851001-th.png' height='300em' width='250em'/>
-            <div className='turnipText'>{turnips}</div>
+            <img src='https://www.models-resource.com/resources/big_icons/36/35107.png' height='300em' width='350em' alt='bells'/>
+            <h1 className='bellText'>{bells}</h1>
+            <img src='https://wyspstore2.s3.amazonaws.com/thumbs/823851001-th.png' height='300em' width='250em' alt='turnips'/>
+            <h1 className='turnipText'>{turnips}</h1>
             </div>
         </div>
     )
