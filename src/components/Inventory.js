47165053b9504dc9f97214215fd7668fc7ca6974
Ryan Aldous <aldous.ryan1@gmail.com>
2020-05-02 20:17:47 -0400

    commit

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -3,8 +3,8 @@ import '../StalkMarket.css'
 
 const Inventory = ( { bells, turnips } ) => {
     return (
-        <div>
-            <div className='container'>
+        <div className='container'>
+            <div>
             <img src='https://www.models-resource.com/resources/big_icons/36/35107.png' height='300em' width='350em' alt='bells'/>
             <h1 className='bellText'>{bells}</h1>
             <img src='https://wyspstore2.s3.amazonaws.com/thumbs/823851001-th.png' height='300em' width='250em' alt='turnips'/>
