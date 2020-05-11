d6a8d24016f3483c47f07d339f18dcdcc6afd1c0
Ryan Aldous <aldous.ryan1@gmail.com>
2020-05-11 12:52:30 -0400

    responsiveness changes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,8 @@ const Logo = ({ day }) => {
         <div>
             <div>
                 <h1 className='logo-font'>STALK MARKET</h1>
-                <h4 className='logo-font-smaller'>Buy turnips to begin playing!</h4>
+                <h4 className='logo-font-smaller' style={{lineHeight: 1}}>Buy turnips to begin playing!
+                                                                    <br/>Get 3000 bells to win!</h4>
                 <h5 className='today'>Today is {day}!</h5>
             </div>
         </div>
